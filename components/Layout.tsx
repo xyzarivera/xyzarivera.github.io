7571612a35638f618af2286237371a037bfd023f
Xyza Rivera <xyzarivera@gmail.com>
2023-03-05 15:36:46 +0800

    Create Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import React, { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      {children}
+    </>
+  );
+}
